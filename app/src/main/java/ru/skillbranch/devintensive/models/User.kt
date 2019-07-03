e056b53a931aa2ad15e02b6d48a653877b941c31
Philipp Stepanenko <finik-st@narod.ru>
2019-07-03 15:29:07 +0300

    hometask_2 first try

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,42 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+data class User(
+    val id : String,
+    var firstName : String?,
+    var lastName : String?,
+    var avatar : String?,
+    var rating : Int = 0,
+    var respect : Int = 0,
+    var lastVisit : Date? = Date(),
+    var isOnline : Boolean = false
+){
+
+    constructor(id : String, firstName : String?, lastName : String?) : this (
+        id = id,
+        firstName = firstName,
+        lastName = lastName,
+        avatar = null
+    )
+
+    constructor(id : String) : this (
+        id, firstName = "John", lastName = "Doe"
+    )
+
+    init{
+        println("I'm Alive!\n" +
+                "${if (lastName === "Doe") "His name is $firstName $lastName" else "And his name is $firstName $lastName!!!"}")
+    }
+
+    companion object Factory{
+        private var lastId: Int = -1
+        fun makeUser(fullName: String?) : User{
+            lastId++
+
+            val (firstName, lastName) = Utils.ParseFullName(fullName)
+            return User(id="$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+}
---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,84 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+
+    val T = mapOf(
+        "а" to "a",
+        "б" to "b",
+        "в" to "v",
+        "г" to "g",
+        "д" to "d",
+        "е" to "e",
+        "ё" to "e",
+        "ж" to "zh",
+        "з" to "z",
+        "и" to "i",
+        "й" to "i",
+        "к" to "k",
+        "л" to "l",
+        "м" to "m",
+        "н" to "n",
+        "о" to "o",
+        "п" to "p",
+        "р" to "r",
+        "с" to "s",
+        "т" to "t",
+        "у" to "u",
+        "ф" to "f",
+        "х" to "h",
+        "ц" to "c",
+        "ч" to "ch",
+        "ш" to "sh",
+        "щ" to "sh'",
+        "ъ" to "",
+        "ы" to "i",
+        "ь" to "",
+        "э" to "e",
+        "ю" to "yu",
+        "я" to "ya")
+
+    fun ParseFullName(fullName : String?) : Pair<String?,String?>{
+        val parts : List<String>? = fullName?.split(" ")
+        var firstName = parts?.getOrNull(0)
+        var lastName = parts?.getOrNull(1)
+        return MustShowString(firstName) to MustShowString(lastName)
+    }
+
+    fun MustShowString(s : String?) : String{
+        when (!s.isNullOrEmpty()){
+            true -> return s
+            false -> return "null"
+        }
+    }
+
+    fun transliteration(payload: String, divider: String = " "): String {
+        var res = ""
+
+        for (char in payload){
+            res += when (char.toString().toLowerCase()){
+                in T -> T[char.toString().toLowerCase()]
+                " " -> divider
+                else -> char.toString()
+            }
+
+        }
+        val parts : List<String>? = res.split(divider)
+        return parts!!.get(0).capitalize() + divider + parts!!.get(1).capitalize()
+    }
+
+    fun toInitials(firstName: String?, lastName: String?): String? {
+        var s = ""
+
+        if (!firstName.isNullOrEmpty() && firstName!=" ") {
+            s += firstName.substring(0,1).toUpperCase()
+        }
+        if (!lastName.isNullOrEmpty() && lastName!=" ") {
+            s += lastName.substring(0,1).toUpperCase()
+        }
+        if (s === ""){
+            s = "null"
+        }
+        return s
+    }
+
+}
