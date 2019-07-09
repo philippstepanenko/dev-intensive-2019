a9a9db73981bd7799254a9ca8f3cdbc780458806
Philipp Stepanenko <finik-st@narod.ru>
2019-07-09 03:02:18 +0300

    hometask_2 fix initials and transliteration

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -2,83 +2,70 @@ package ru.skillbranch.devintensive.utils
 
 object Utils {
 
-    val T = mapOf(
-        "а" to "a",
-        "б" to "b",
-        "в" to "v",
-        "г" to "g",
-        "д" to "d",
-        "е" to "e",
-        "ё" to "e",
-        "ж" to "zh",
-        "з" to "z",
-        "и" to "i",
-        "й" to "i",
-        "к" to "k",
-        "л" to "l",
-        "м" to "m",
-        "н" to "n",
-        "о" to "o",
-        "п" to "p",
-        "р" to "r",
-        "с" to "s",
-        "т" to "t",
-        "у" to "u",
-        "ф" to "f",
-        "х" to "h",
-        "ц" to "c",
-        "ч" to "ch",
-        "ш" to "sh",
-        "щ" to "sh'",
-        "ъ" to "",
-        "ы" to "i",
-        "ь" to "",
-        "э" to "e",
-        "ю" to "yu",
-        "я" to "ya")
-
     fun parseFullName(fullName : String?) : Pair<String?,String?>{
-        val parts : List<String>? = fullName?.split(" ")
+        val parts : List<String>? = fullName?.trim()?.split(" ")
         var firstName = parts?.getOrNull(0)
         var lastName = parts?.getOrNull(1)
         return MustShowString(firstName) to MustShowString(lastName)
     }
 
-    fun MustShowString(s : String?) : String?{
-        when (!s.isNullOrEmpty()){
-            true -> return s
-            false -> return null
+    private fun MustShowString(s : String?) : String?{
+        return when (!s.isNullOrEmpty()){
+            true -> s
+            false -> null
         }
-    }
-
-    fun transliteration(payload: String, divider: String = " "): String {
-        var res = ""
-
-        for (char in payload){
-            res += when (char.toString().toLowerCase()){
-                in T -> T[char.toString().toLowerCase()]
-                " " -> divider
-                else -> char.toString()
-            }
-
-        }
-        val parts : List<String>? = res.split(divider)
-        return parts!!.get(0).capitalize() + divider + parts!!.get(1).capitalize()
     }
 
     fun toInitials(firstName: String?, lastName: String?): String? {
-        var s = ""
+        var s : String? = ""
 
-        if (!firstName.isNullOrEmpty() && firstName!=" ") {
-            s += firstName.substring(0,1).toUpperCase()
+        if (!firstName.isNullOrEmpty() && firstName?.trim() != "") {
+            s += "${firstName.trim()}"[0].toUpperCase()
         }
-        if (!lastName.isNullOrEmpty() && lastName!=" ") {
-            s += lastName.substring(0,1).toUpperCase()
+        if (!lastName.isNullOrEmpty() && lastName?.trim() != "") {
+            s += "${lastName.trim()}"[0].toUpperCase()
         }
         if (s === ""){
-            s = "null"
+            s = null
         }
         return s
     }
+
+
+    fun transliteration(payload: String, divider: String = " ") = payload.map {
+        val res = when (it.toLowerCase()) {
+            'а' -> "a"
+            'б' -> "b"
+            'в' -> "v"
+            'г' -> "g"
+            'д' -> "d"
+            'е', 'ё', 'э' -> "e"
+            'ж' -> "zh"
+            'з' -> "z"
+            'и', 'й', 'ы' -> "i"
+            'к' -> "k"
+            'л' -> "l"
+            'м' -> "m"
+            'н' -> "n"
+            'о' -> "o"
+            'п' -> "p"
+            'р' -> "r"
+            'с' -> "s"
+            'т' -> "t"
+            'у' -> "u"
+            'ф' -> "f"
+            'х' -> "h"
+            'ц' -> "c"
+            'ч' -> "ch"
+            'ш' -> "sh"
+            'щ' -> "sh'"
+            'ъ', 'ь' -> ""
+            'ю' -> "yu"
+            'я' -> "ya"
+            ' ' -> divider
+            else -> "$it"
+        }
+        if (it.isUpperCase()) res.capitalize() else res
+    }.joinToString("")
 
 }
