4a8b6189a85918f2fb08c2c403702b0373e3e510
Philipp Stepanenko <finik-st@narod.ru>
2019-07-17 23:16:06 +0300

    hometask_3 fix task3

--- app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/Bender.kt
@@ -16,10 +16,14 @@ class Bender(var status: Status = Status.NORMAL, var question: Question = Questi
     fun listenAnswer(answer:String) : Pair<String, Triple<Int, Int, Int>>{
         return if (question.answers.contains(answer)){
             question = question.nextQuestion()
-            "Отлично - это правильный ответ!\n${question.question}" to status.color
+            "Отлично - ты справился\n${question.question}" to status.color
         }else{
             status = status.nextStatus()
-            "Это не правильный ответ!\n${question.question}" to status.color
+            if (status.name == Status.NORMAL.name){
+                "Это неправильный ответ. Давай все по новой\n${question.question}" to status.color
+            }else{
+                "Это неправильный ответ\n${question.question}" to status.color
+            }
         }
 
     }
